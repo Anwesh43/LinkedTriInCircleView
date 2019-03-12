b2521c9a1bad4475f4b8a31a1a3d3495c576fd05
Anwesh43 <anweshthecool0@gmail.com>
2019-03-12 09:58:50 +0530

    adding State class

--- triincircleview/src/main/java/com/anwesh/uiprojects/triincircleview/TriInCircleView.kt
+++ triincircleview/src/main/java/com/anwesh/uiprojects/triincircleview/TriInCircleView.kt
@@ -60,7 +60,7 @@ fun Canvas.drawTICNode(i : Int, scale : Float, paint : Paint) {
 class TriInCircleView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    
+
     override fun onDraw(canvas : Canvas) {
 
     }
@@ -72,5 +72,25 @@ class TriInCircleView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scale.updateValue(dir, tri, 1)
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
