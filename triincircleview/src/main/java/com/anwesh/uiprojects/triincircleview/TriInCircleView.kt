9b7a36285fce1ad66417a19ba35e3cebe33c3094
Anwesh43 <anweshthecool0@gmail.com>
2019-03-12 09:11:29 +0530

    adding librraries and adding constants

--- triincircleview/src/main/java/com/anwesh/uiprojects/triincircleview/TriInCircleView.kt
+++ triincircleview/src/main/java/com/anwesh/uiprojects/triincircleview/TriInCircleView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.triincircleview
 /**
  * Created by anweshmishra on 12/03/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Color
+
+val nodes : Int = 5
+val tri : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val triColor : Int = Color.parseColor("#673AB7")
+val circleColor : Int = Color.parseColor("#BDBDBD")
+val fillColor : Int = Color.parseColor("#212121")
