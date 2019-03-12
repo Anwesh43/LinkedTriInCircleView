90e08863e9a90ecc8725aacf2fd59262aee7f74c
Anwesh43 <anweshthecool0@gmail.com>
2019-03-12 10:34:57 +0530

    adding TICNode class

--- triincircleview/src/main/java/com/anwesh/uiprojects/triincircleview/TriInCircleView.kt
+++ triincircleview/src/main/java/com/anwesh/uiprojects/triincircleview/TriInCircleView.kt
@@ -121,4 +121,48 @@ class TriInCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TICNode(var i : Int, val state : State = State()) {
+
+        private var next : TICNode? = null
+        private var prev : TICNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TICNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTICNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TICNode {
+            var curr : TICNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
